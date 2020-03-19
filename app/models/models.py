e68e3e3f49bf36f72d4eaa13e8585103580c27ba
nkartha2 <nkartha2@gmail.com>
2020-03-19 16:18:26 -0500

    wip db models and table

--- app/models/models.py
+++ app/models/models.py
@@ -1,11 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Integer, String
+from sqlalchemy import Model, Table, Integer, String, Column, Boolean, DateTime, ForeignKey
+import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = ''
 db = SQLAlchemy(app)
 
-class Movie(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  title = db.Column(db.String)
+class Movie(Model):
+  id = Column(Integer, primary_key=True)
+  title = Column(String)
+  director = Column(String)
+  watched = Column(Boolean)
+  created_at = Column(DateTime, default=datetime.datetime.utcnow())
+
+class Artist(Model):
+  id = Column(Integer, primary_key=True)
+  name = Column(String)
+
+class Recommendation(Table):
+  id = Column(Integer, primary_key=True)
+  artist_id = Column(Integer, ForeignKey('artist.id'), primary_key=True)
+  movie_id = Column(Integer, ForeignKey('movie.id'), primary_key=True)
