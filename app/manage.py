0086e6ef23473eae0c07a4f8af8962a39b9b1620
nkartha2 <nkartha2@gmail.com>
2020-04-13 13:58:11 -0500

    fix import

--- app/manage.py
+++ app/manage.py
@@ -1,7 +1,7 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-import app
+from app import app
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
