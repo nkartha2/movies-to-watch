38d0cf5ea7e9246ccead5abe1bf66c66783393cc
nkartha2 <nkartha2@gmail.com>
2020-04-13 12:08:25 -0500

    fixing import error

--- app/manage.py
+++ app/manage.py
@@ -1,10 +1,9 @@
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
-from app import app, db
+import app
 
 manager = Manager(app)
-
 manager.add_command('db', MigrateCommand)
 
 
