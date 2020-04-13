9fc451d66ff0e6fe7300b9cc3d453d154005d974
nkartha2 <nkartha2@gmail.com>
2020-04-13 13:50:16 -0500

    rename to be less confusing

---
+++ app/run.py
@@ -0,0 +1,19 @@
+from app import app, db
+from app.views import recommendation_views
+from flask import Flask
+from flask_cors import CORS, cross_origin
+from app.models import *
+
+
+# Define WSGI app object
+CORS(app)
+
+app.register_blueprint(recommendation_views)
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+# https://www.digitalocean.com/community/tutorials/how-to-structure-large-flask-applications
+if __name__ == "__main__":
+  app.run()
