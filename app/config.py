9adce1ceaebf0357ddd25c030d10fd09a52c0969
nkartha2 <nkartha2@gmail.com>
2020-04-15 22:15:32 -0500

    clean up config

--- app/config.py
+++ app/config.py
@@ -3,8 +3,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Enables development environment
 DEBUG = True
-# DATABASE_URI = 'postgres+psycopg2://postgres:postgres@127.0.0.1:5433/movies_to_watch'
-
 
 class Config(object):
     DEBUG = False
